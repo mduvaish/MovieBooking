d3cd65906282857db0101e65f0fdfe28593e355a
mduvaish <42836943+mduvaish@users.noreply.github.com>
2024-10-30 15:57:07 -0400

    Update MovieBookingSystem.cpp

--- MovieBookingSystem.cpp
+++ MovieBookingSystem.cpp
@@ -1,32 +1,22 @@
 #include "MovieBookingSystem.h"
 #include <iostream>
 
-void MovieBookingSystem::addTheater(const std::string& theaterName, const std::string& movieTitle, int seats) {
-    theaters.emplace_back(theaterName, Movie(movieTitle), seats);
-    std::cout << "Theater \"" << theaterName << "\" added with movie \"" << movieTitle 
-              << "\" and " << seats << " seats.\n";
+void MovieBookingSystem::addTheater(const std::string& theaterName, const std::string& movieTitle) {
+    theaters.emplace_back(theaterName, Movie(movieTitle));
+    std::cout << "Theater \"" << theaterName << "\" added with movie \"" << movieTitle << "\".\n";
 }
 
 void MovieBookingSystem::displayTheaters() const {
-    std::cout << "Available theaters and movies:\n";
     for (const auto& theater : theaters) {
-        std::cout << "Theater: " << theater.getName() 
-                  << " | Movie: " << theater.getMovie().getTitle() 
-                  << " | Seats available: " << theater.getAvailableSeats() << "\n";
+        std::cout << "Theater: " << theater.getName() << " | Movie: " << theater.getMovie().getTitle() << "\n";
+        theater.displayAvailableSeats();
     }
 }
 
-bool MovieBookingSystem::bookMovieInTheater(const std::string& theaterName, int seats) {
+bool MovieBookingSystem::bookSeatsInTheater(const std::string& theaterName, const std::vector<int>& seatNumbers) {
     for (auto& theater : theaters) {
         if (theater.getName() == theaterName) {
-            if (theater.bookSeats(seats)) {
-                std::cout << "Successfully booked " << seats << " seats at " << theaterName << " for movie "
-                          << theater.getMovie().getTitle() << ".\n";
-                return true;
-            } else {
-                std::cout << "Not enough seats available at " << theaterName << ".\n";
-            }
-            return false;
+            return theater.bookSeats(seatNumbers);
         }
     }
     std::cout << "Theater not found.\n";
