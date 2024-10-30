cb2f1a67e4bf5b9c46b3e31603641b0b1666eac4
mduvaish <42836943+mduvaish@users.noreply.github.com>
2024-10-30 11:52:39 -0400

    Update Movie.cpp

--- Movie.cpp
+++ Movie.cpp
@@ -1,6 +1,6 @@
 #include "Movie.h"
 
-Movie::Movie(const std::string& title, int availableSeats)     : title(title), availableSeats(availableSeats) {}
+Movie::Movie(const std::string& title) : title(title) {}
+
 const std::string& Movie::getTitle() const { return title; }
-int Movie::getAvailableSeats() const { return availableSeats; }
-bool Movie::bookSeats(int seats) {    if (seats <= availableSeats) {        availableSeats -= seats;        return true;    }    return false;}
+
